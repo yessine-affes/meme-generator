b04f01cc557761f9760c5a59830bb94c290b3e30
yessine <affesyessine90@gmail.com>
2024-10-19 16:51:08 +0100

    first

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+import './App.css'
+import Header from "./components/header"
+import Meme from "./components/meme"
+
+export default function App() {
+
+   
+  
+  
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  )
+}
+
+
